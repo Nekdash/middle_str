2b2d2d515151ba935c621039c4d744252a908e86
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-18 07:35:17 +0300

    fgh

--- itc_DecToBin.cpp
+++ itc_DecToBin.cpp
@@ -5,10 +5,17 @@ string itc_DecToBin(string str){
     string temp = "";
     while (str[i] != '\0'){
         if ( itc_isDigit(str[i])){
+                temp = "";
             for ( long long j = i; itc_isDigit(str[j]); j++){
                 temp += str[j];
             }
         temp_num = str_to_num(temp);
+        temp = itc_decToBase(temp_num, 2);
+        str = itc_slice_str(str, 0, i -1) + temp + itc_slice(str, j, itc_len(str));
+        i = j;
+        }else{
+            i++;
         }
     }
+    return str;
 }
