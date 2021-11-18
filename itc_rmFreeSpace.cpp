50cda83bd40b0e93cd40cd8f919270994f3d2e0a
Nekdash <78729514+Nekdash@users.noreply.github.com>
2021-11-18 12:42:33 +0300

    Update itc_rmFreeSpace.cpp

--- itc_rmFreeSpace.cpp
+++ itc_rmFreeSpace.cpp
@@ -2,15 +2,15 @@
 
 string itc_rmFreeSpace(string str){
     long long i = 0;
-
+    long long addon = 0;
     while(str[i] != '\0'){
         if (str[i] == ' '){
             for (long long j = i; str[j] != ' '; j++){
-
+                addon = j;
             }
-            str = itc_slice_str(str, 0, i) + itc_slice_str(str, j, itc_len(str));
+            str = itc_slice_str(str, 0, i) + itc_slice_str(str, addon + 1, itc_len(str));
         }
-        i =
+        i++;
     }
 
 }
